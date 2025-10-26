9d5daa770e807dad5668758a3c1fc078674988cf
jasmine <jasnaminemine@gmail.com>
2025-10-26 16:18:21 +0700

    fix: delete modules product, drone

--- backend/src/modules/drone/drone.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Controller } from '@nestjs/common';
-import { DroneService } from './drone.service';
-
-@Controller('drone')
-export class DroneController {
-  constructor(private readonly droneService: DroneService) {}
-}

6d7466107129b2dd2ca9cfb543447c28bde08626
ThanhTam3h <dao.thi.thanh.tam.0807@gmail.com>
2025-10-30 15:20:18 +0700

    Setup Redux Toolkit and userSlice

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,13 +8,28 @@ import {
 import Category from './pages/Category';
 import Login from './pages/customerLogin/Login';
 import Register from './pages/customerLogin/Register';
+import CustomerHome from './pages/customerLogin/CustomerHome';
+export const serverUrl = 'http://localhost:3000';
+
 function App() {
+  const user = JSON.parse(localStorage.getItem('user')); // kiểm tra đã đăng nhập
+
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Navigate to="/login" replace />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
+        <Route
+          path="/login"
+          element={!user ? <Login /> : <Navigate to="/home" replace />}
+        />
+        <Route
+          path="/register"
+          element={!user ? <Register /> : <Navigate to="/home" replace />}
+        />
+        <Route
+          path="/home"
+          element={user ? <CustomerHome /> : <Navigate to="/login" replace />}
+        />
       </Routes>
     </Router>
   );

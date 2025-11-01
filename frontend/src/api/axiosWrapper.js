135e77e6862c200b3fa51b2853476b085b98a35c
ThanhTam3h <dao.thi.thanh.tam.0807@gmail.com>
2025-11-02 00:29:13 +0700

    refactor(api): update axios wrapper to support auth token

--- frontend/src/api/axiosWrapper.js
+++ frontend/src/api/axiosWrapper.js
@@ -1,9 +1,9 @@
-import axios from "axios";
+import axios from 'axios';
 export const axiosWrapper = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: 'http://localhost:3000/api/v1',
   withCredentials: true,
   headers: {
-    "Content-Type": "application/json",
-    Accept: "application/json",
+    'Content-Type': 'application/json',
+    Accept: 'application/json',
   },
 });

70c4e0222f02047087b173b0e625ca49e6fedf85
ThanhTam3h <dao.thi.thanh.tam.0807@gmail.com>
2025-11-19 01:58:16 +0700

    refactor(admin): update admin routes to use new OrderPage component

--- frontend-react/src/Admin/Admin.jsx
+++ frontend-react/src/Admin/Admin.jsx
@@ -3,8 +3,8 @@ import { Route, Routes } from "react-router-dom";
 
 import { useDispatch, useSelector } from "react-redux";
 import {
-  getIngredientCategory,
-  getIngredientsOfRestaurant,
+	getIngredientCategory,
+	getIngredientsOfRestaurant,
 } from "../State/Admin/Ingredients/Action";
 import { fetchRestaurantsOrder } from "../State/Admin/Order/restaurants.order.action";
 import { getRestaurantsCategory } from "../State/Customers/Restaurant/restaurant.action";
@@ -18,61 +18,62 @@ import RestaurantsMenu from "./Food/RestaurantsMenu";
 import Ingredients from "./Ingredients/Ingredients";
 import RestaurantsOrder from "./Orders/RestaurantsOrder";
 import RestaurantDashboard from "./pages/RestaurantDashboard";
+import { OrdersPage } from "./pages/OrdersPage";
 
 const Admin = () => {
-  const dispatch = useDispatch();
-  const [openSideBar, setOpenSideBar] = useState(false);
-  const handleOpenSideBar = () => setOpenSideBar(true);
-  const handleCloseSideBar = () => setOpenSideBar(false);
-  const { auth, restaurant, ingredients } = useSelector((store) => store);
-  const jwt = localStorage.getItem("jwt");
-  useEffect(() => {
-    if (restaurant.usersRestaurant) {
-      dispatch(
-        getIngredientCategory({ jwt, id: restaurant.usersRestaurant?.id })
-      );
-      dispatch(
-        getIngredientsOfRestaurant({ jwt, id: restaurant.usersRestaurant?.id })
-      );
-      dispatch(
-        getRestaurantsCategory({
-          jwt: auth.jwt || jwt,
-          restaurantId: restaurant.usersRestaurant?.id,
-        })
-      );
+	const dispatch = useDispatch();
+	const [openSideBar, setOpenSideBar] = useState(false);
+	const handleOpenSideBar = () => setOpenSideBar(true);
+	const handleCloseSideBar = () => setOpenSideBar(false);
+	const { auth, restaurant, ingredients } = useSelector((store) => store);
+	const jwt = localStorage.getItem("jwt");
+	useEffect(() => {
+		if (restaurant.usersRestaurant) {
+			dispatch(
+				getIngredientCategory({ jwt, id: restaurant.usersRestaurant?.id })
+			);
+			dispatch(
+				getIngredientsOfRestaurant({ jwt, id: restaurant.usersRestaurant?.id })
+			);
+			dispatch(
+				getRestaurantsCategory({
+					jwt: auth.jwt || jwt,
+					restaurantId: restaurant.usersRestaurant?.id,
+				})
+			);
 
-      dispatch(
-        fetchRestaurantsOrder({
-          restaurantId: restaurant.usersRestaurant?.id,
-          jwt: auth.jwt || jwt,
-        })
-      );
-    }
-  }, [restaurant.usersRestaurant]);
-  return (
-    <div>
-      <AdminNavbar handleOpenSideBar={handleOpenSideBar} />
-      <div className="lg:flex justify-between">
-        {/* <div className="">
+			dispatch(
+				fetchRestaurantsOrder({
+					restaurantId: restaurant.usersRestaurant?.id,
+					jwt: auth.jwt || jwt,
+				})
+			);
+		}
+	}, [restaurant.usersRestaurant]);
+	return (
+		<div>
+			<AdminNavbar handleOpenSideBar={handleOpenSideBar} />
+			<div className="lg:flex justify-between">
+				{/* <div className="">
           <AdminSidebar handleClose={handleCloseSideBar} open={openSideBar} />
         </div> */}
 
-        <div className="w-full">
-          <Routes>
-            <Route path="/" element={<RestaurantDashboard />} />
-            <Route path="/orders" element={<RestaurantsOrder />} />
-            <Route path="/menu" element={<RestaurantsMenu />} />
-            <Route path="/add-menu" element={<AddMenuForm />} />
-            <Route path="/add-restaurant" element={<CreateRestaurantForm />} />
-            <Route path="/event" element={<IngredientTable />} />
-            <Route path="/ingredients" element={<Ingredients />} />
-            <Route path="/category" element={<Category />} />
-            <Route path="/details" element={<Details />} />
-          </Routes>
-        </div>
-      </div>
-    </div>
-  );
+				<div className="w-full">
+					<Routes>
+						<Route path="/" element={<RestaurantDashboard />} />
+						<Route path="/orders" element={<OrdersPage />} />
+						<Route path="/menu" element={<RestaurantsMenu />} />
+						<Route path="/add-menu" element={<AddMenuForm />} />
+						<Route path="/add-restaurant" element={<CreateRestaurantForm />} />
+						<Route path="/event" element={<IngredientTable />} />
+						<Route path="/ingredients" element={<Ingredients />} />
+						<Route path="/category" element={<Category />} />
+						<Route path="/details" element={<Details />} />
+					</Routes>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Admin;
